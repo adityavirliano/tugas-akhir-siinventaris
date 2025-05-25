5182e453ef83172b1a5f730baea8d99bab88eaed
adityavirliano <virlianoaditya@gmail.com>
2025-05-26 01:17:25 +0700

    Update cek.php

--- SistemInformasiInventaris/cek.php
+++ SistemInformasiInventaris/cek.php
@@ -1,9 +1,9 @@
 <?php
-//jika blm login
-
-if(isset($_SESSION['log'])){
-
-}else{
+//jika belum login tidak bisa masuk index
+if(!isset($_SESSION['log'])){
     header('location:login.php');
 }
+
+// Ambil peran pengguna dari sesi, default ke 'guest' jika tidak ada
+$role = isset($_SESSION['role']) ? $_SESSION['role'] : 'guest';
 ?>
